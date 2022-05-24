008669bedf56cda90703306178aee4196965e877
Loktrbl <conca8048@gmail.com>
2022-05-24 21:23:43 +0700

    Thêm testconnect vào qltv context trong DAL

--- DA_PTUD_QLTV/DAL/TestDAL.cs
+++ DA_PTUD_QLTV/DAL/TestDAL.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ProjectSettings.Properties; // Using cái này đầu tiên
+using DTO;
 
 namespace DAL
 {
@@ -14,6 +15,7 @@ namespace DAL
         /// để kết nối cơ sở dữ liệu, việc thay đổi UserConnectionString sẽ tiện cho việc thay thế toàn bộ kết nối nhanh chóng hơn
         /// </summary>
         string TestConnectionString = Settings.Default.UserConnectionString;
+        DB_PTUD_QLTVDataContext qltv = new DB_PTUD_QLTVDataContext(Settings.Default.UserConnectionString);
 
        
 
